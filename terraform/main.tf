0ebe490dfa650c60e59783c5d2d56fe1c2d93c57
root <root@ip-172-31-9-111.ap-southeast-2.compute.internal>
2023-10-19 11:20:35 +0000

    makechanges

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,8 +4,9 @@ access_key = "AKIAXFOFGNIUFQGWP7N2"
 secret_key = "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"
 }
 resource "aws_instance" "key" {
-ami = " "
-instance_type ="t2.micro"
+ami = "ami-023e1b810cda5ce69"
+instance_type = "t2.micro"
+key_name = "oct19"
 vpc_security_group_ids = [aws_security_group.demo-sg.id]
 tags= {
 name = "auto-instance"
